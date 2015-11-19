00e004278b1e3e50c81cf203f6ef677335f2dc9d
Hendrik Luehrsen <hendrik@luehrsen.de>
2015-11-19 09:45:46 +0100

    Workspace Setup

---
+++ Gruntfile.js
@@ -0,0 +1,156 @@
+module.exports = function(grunt) {
+  require('jit-grunt')(grunt);
+  grunt.template.addDelimiters('underscoresaving', '<##', '##>');
+  grunt.template.setDelimiters('underscoresaving');
+  grunt.initConfig({
+
+  	// Define variables
+    pkg:     grunt.file.readJSON("package.json"),
+
+	// LESS / CSS
+
+	// Compile Less
+	// Compile the less files
+    less: {
+      development: {
+        options: {
+          compress: true,
+          yuicompress: true,
+          optimization: 2
+        },
+        files: {
+          "build/css/admin.css": "build/less/admin.less" // destination file and source file
+        }
+      }
+    },
+
+
+    autoprefixer: {
+		options: {
+			// Task-specific options go here.
+		},
+		style: {
+            src: 'build/css/admin.css',
+			dest: 'build/css/admin.css'
+   		}
+	},
+
+    // JAVASCRIPT
+
+    // JS HINT
+    // How's our code quality
+    jshint: {
+	    options: {
+			reporter: require('jshint-stylish'),
+			force: true,
+	    },
+    	all: ['build/js/**/*.js', '!build/js/**/*.min.js', '!build/js/bootstrap/**/*.js', '!build/js/vendor/**/*.js']
+  	},
+
+    // Concat
+    // Join together the needed files.
+	concat_in_order: {
+		main: {
+			files: {
+				'build/js/afb_ia.min.js': ['build/js/afb_ia.js']
+			},
+			options: {
+			    extractRequired: function(filepath, filecontent) {
+				    var path = require('path');
+
+			        var workingdir = path.normalize(filepath).split(path.sep);
+			        workingdir.pop();
+
+			        var deps = this.getMatches(/@depend\s"(.*\.js)"/g, filecontent);
+			        deps.forEach(function(dep, i) {
+			            var dependency = workingdir.concat([dep]);
+			            deps[i] = path.join.apply(null, dependency);
+			        });
+			        return deps;
+			    },
+			    extractDeclared: function(filepath) {
+			        return [filepath];
+			    },
+			    onlyConcatRequiredFiles: true
+			}
+		}
+	},
+
+	// Uglify
+	// We minify the files, we just concatenated
+	uglify: {
+	    mstartup: {
+	      options: {
+	      },
+	      files: {
+			'build/js/afb_ia.min.js': ['build/js/afb_ia.min.js']
+	      }
+	    }
+	},
+
+	// WATCHER / SERVER
+
+    // Watch
+    watch: {
+	    js: {
+		    files: ['build/js/**/*.js', '!build/js/**/*.min.js'],
+		    tasks: ['deploy'],
+			options: {
+				livereload: true
+			},
+	    },
+		less: {
+			files: ['build/css/**/*.less'], // which files to watch
+			tasks: ['deploy'],
+			options: {
+				// livereload: true
+			},
+		},
+		css: {
+			files: ['build/**/*.css', 'build/*.css', ],
+			tasks: [],
+			options: {
+				livereload: true
+			}
+		},
+		livereload: {
+			files: ['build/js/*.min.js', 'build/**/*.php', 'build/**/*.html', 'build/**/*.txt'], // Watch all files
+			tasks: ['deploy'],
+			options: {
+				livereload: true
+			}
+		},
+    },
+
+    // Deployment Strategies
+
+    // Copy the files to the target destination
+    copy: {
+	    options : {
+          processContent: function (content, srcpath) {
+			grunt.template.addDelimiters('custom-delimiters', '<##', '##>');
+            return grunt.template.process(content, {delimiters: 'custom-delimiters'});
+          }
+	    },
+	    test_plugin: {expand: true, cwd: 'build', src: ['**/*.php', '**/*.min.js', '**/*.css', '**/*.txt','**/*.svg', '!node_modules', '!node_modules/**/*'], dest: 'trunk/', filter: 'isFile'}
+    },
+
+    // Clean out folders
+    clean: {
+		options: { force: true },
+		build: {
+			expand: true,
+			force: true,
+		  	cwd: "trunk/",
+			src: ['**/*'],
+		},
+	}
+  });
+
+  grunt.registerTask( 'handle_css', ['less', 'autoprefixer', 'uglify'] );
+  grunt.registerTask( 'handle_js', ['concat_in_order', 'uglify'] );
+  grunt.registerTask( 'dev_js', ['concat_in_order'] );
+  grunt.registerTask( 'deploy', ['handle_css', 'handle_js', 'clean', 'copy'] );
+
+};
+
---
+++ trunk/readme.txt
@@ -0,0 +1,72 @@
+=== allfacebook.de Instant Articles ===
+Contributors: luehrsen, jwiese
+Tags: free, images, image, pictures, media, import, creative commons, free images
+Requires at least: 4.0
+Tested up to: 4.4
+Stable tag: 1.0.0
+License: GNU General Public License v2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+This plugin integrates the free-images.cc image library directly into your media browser.
+
+== Description ==
+
+Over the last years the creative commons movement gained traction and more and more artists followed the trend of committing their work to public domain. We believe that this will contribute to making the web a better, more beautiful place.
+
+But we don’t want to be one of many. There are several aspects, that will make [free-images.cc](http://www.free-images.cc) superior to many other sites out there. Not only do we curate the images and publish only the creme de la creme. Every image is carefully tagged for better discoverability and every photographer is named and honored.
+
+Every free image uploaded here will be under the CC0 Public Domain license. So every image will be free to use for whatever purpose you want!
+
+We are always keen for improvement and new artists, that publish their work under the creative commons public domain license. If you have any feedback or tips for great, free images, drop us a mail at contact@free-images.cc!
+
+This plugin is brought to you by the awesome folks at [Luehrsen // Heinrich](http://www.luehrsen-heinrich.de).
+
+== Installation ==
+
+1. Upload the fap_importer folder to the /wp-content/plugins/ directory
+2. Activate the 'free-images.cc Importer' plugin through the 'Plugins' menu in WordPress
+3. Import your first image in the admin media screen
+
+== Frequently Asked Questions ==
+
+= Where are these images coming from? =
+
+All of our images are coming from incredibly talented photographers from all over the world. The released their work to public domain and that allows us to add it to the library, to bring these works directly into your WordPress backend.
+
+= Why are these images free? =
+
+Because they are released under the [CC0 Public Domain license](https://creativecommons.org/publicdomain/zero/1.0/). The photographer made them free to use for everyone around the world.
+
+=======
+
+= Are there any restrictions on the usage? =
+
+Technically no. That means, that you can do whatever you want with these images without having to give attribution to the photographer or free-images.cc.
+
+We just kindly ask you not to use these images for pornographic, criminal, defamatory or degrading purposes.
+
+
+== Screenshots ==
+
+1. The "free images" tab shows up directly in your media browser.
+
+== Changelog ==
+=======
+= 0.6.0 =
+Added a live-search.
+Added lazy load depending on the viewport.
+Added a preview button within the sidebar.
+Implemented client-caching to prevent long loads and/or timeouts.
+Rewrote part of the JS-Core for a better workflow.
+Changed template behavior.
+Fixed a bug where our upload-spinner appeared in new opened media-frames sometimes.
+
+= 0.5.2 =
+Frame reload fix to ensure a reload of the local media collection.
+Small template changes.
+
+= 0.5.1 =
+Template fix to work proper with php shorttag.
+
+= 0.5 =
+Initial Release
