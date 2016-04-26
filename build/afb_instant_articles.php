ed4579534fed4ec782a2d9c6126209e30ef478ae
Hendrik Luehrsen <hendrik@luehrsen.de>
2016-04-26 16:13:15 +0200

    Updated embed code

--- build/afb_instant_articles.php
+++ build/afb_instant_articles.php
@@ -32,6 +32,7 @@ require_once LHAFB__PLUGIN_DIR . "inc/lhafb_ia.core.php"; // The actual core of 
 require_once LHAFB__PLUGIN_DIR . "inc/lhafb_ia.metaboxes.php"; // The metaboxes, that are loaded in the admin views
 require_once LHAFB__PLUGIN_DIR . "inc/lhafb_ia.settings.php"; // The settings options needed for the plugin
 require_once LHAFB__PLUGIN_DIR . "inc/lhafb_ia.filters.php"; // The filters needed to morph the code for instant articles
+require_once LHAFB__PLUGIN_DIR . "inc/embeds.php"; // The embeds filter
 
 // Initialize the main plugin class
 function run_instant_articles(){
